addae2f3af1f846b6fb6d4112fc5026b6162685b
Lukie Kang <lukiek+f@gmail.com>
2019-05-10 16:28:29 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -178,12 +178,12 @@ describe "#display_board in 'lib/display_board.rb" do
     it 'prints an entire board full of Os' do
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
-      board = ["o", "o", "o", "o", "o", "o", "o", "o", "o"] 
+      board = ["o", "o", "o", "o", "o", "o", "o", "o", "o"]
 
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
-      
+
       # Hint: You should be able to copy the code in the previous it eoample
       # and make a few simple edits to convert the previous example to this
       # example's situation.
